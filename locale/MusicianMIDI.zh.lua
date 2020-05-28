0b3e604b729171adb757e3d48346d5b77c9ad5a5
Kalevalar <43042991+Kalevalar@users.noreply.github.com>
2020-05-28 21:03:50 +0200

    Chinese localization

---
+++ locale/MusicianMIDI.zh.lua
@@ -0,0 +1,13 @@
+MusicianMIDI.Locale.zh = Musician.Utils.DeepCopy(MusicianMIDI.Locale.en)
+local msg = MusicianMIDI.Locale.zh
+
+msg.MENU_MIDI_KEYBOARD = "启动MIDI键盘"
+msg.MIDI_KEYBOARD_TITLE = "MIDI键盘"
+msg.SELECT_INSTRUMENT = "选择乐器"
+msg.INSTRUMENT_OCTAVE = "八度移调"
+
+msg.ERROR_INCOMPATIBLE_MUSICIAN_VERSION = "你的Musician版本太旧以致不能使用Musician MIDI，请升级。"
+
+if (GetLocale() == "zhCN" or GetLocale() == "zhTW") then
+	MusicianMIDI.Msg = msg
+end
