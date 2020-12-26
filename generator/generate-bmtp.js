219f3324641eddaf018863e2da1721a3f8c3c04a
LenweSaralonde <lenwe@lenwe.info>
2020-12-26 17:22:05 +0100

    Simplified translator

--- generator/generate-bmtp.js
+++ generator/generate-bmtp.js
@@ -19,17 +19,6 @@ const KEY_NIBBLES = [
 	'12D32D', // Insert
 ];
 
-const MESSAGES_2 = [
-	'C', // Cx pp      Program change
-];
-
-const MESSAGES_3 = [
-	'8', // 8x nn vv   Note nn off
-	'9', // 9x nn vv   Note nn on, Volume vv
-	'B', // Bx nn vv   Controller
-	'E', // Ex v1 v2   Pitch Bender
-];
-
 const KEYSTROKE_COMMAND = 'KAM10100KSQ10004'; // 2 keystrokes
 
 /**
@@ -75,44 +64,46 @@ function getPreset(presetId, presetName, rules) {
  */
 function main() {
 
-	// status rules
-	const rules_2_status = [];
-	const rules_3_status = [];
+	const message_rules = [];
+
 	for (let channel = 0; channel < 16; channel++) {
 		const channelHex = channel.toString(16).toUpperCase();
-		for (let opcodeHex of MESSAGES_2) {
-			const opcode = parseInt(opcodeHex, 16);
-			setRule(rules_2_status, `${opcodeHex}${channelHex}`, `${opcodeHex}${channelHex}pp`, KEYSTROKE_COMMAND + KEY_NIBBLES[opcode] + KEY_NIBBLES[channel]);
-		}
-		for (let opcodeHex of MESSAGES_3) {
-			const opcode = parseInt(opcodeHex, 16);
-			setRule(rules_3_status, `${opcodeHex}${channelHex}`, `${opcodeHex}${channelHex}ppqq`, KEYSTROKE_COMMAND + KEY_NIBBLES[opcode] + KEY_NIBBLES[channel]);
-		}
+
+		// 8x nn vv   Note off
+		setRule(message_rules, `Note off ch.${channel}`, `8${channelHex}ppqq`, KEYSTROKE_COMMAND + KEY_NIBBLES[0x8] + KEY_NIBBLES[channel]);
+
+		// 9x nn vv   Note on
+		setRule(message_rules, `Note on ch.${channel}`, `9${channelHex}ppqq`, KEYSTROKE_COMMAND + KEY_NIBBLES[0x9] + KEY_NIBBLES[channel]);
+
+		// Bx cc vv   Control change
+		setRule(message_rules, `Pedal off ch.${channel}`, `B${channelHex}ppqq`, KEYSTROKE_COMMAND + KEY_NIBBLES[0xB] + KEY_NIBBLES[channel]);
+
+		// Ex v1 v2   Pitch Bender
+		setRule(message_rules, `Pitch bend ch.${channel}`, `E${channelHex}ppqq`, KEYSTROKE_COMMAND + KEY_NIBBLES[0xE] + KEY_NIBBLES[channel]);
+
+		// Cx pp      Program change
+		setRule(message_rules, `Program change ch.${channel}`, `C${channelHex}pp`, KEYSTROKE_COMMAND + KEY_NIBBLES[0xC] + KEY_NIBBLES[channel]);
 	}
 
-	// data rules
-	const rules_2_data = [];
-	const rules_3_data1 = [];
-	const rules_3_data2 = [];
-	for (let value = 0; value < 256; value++) {
+	const value_rules1 = [];
+	const value_rules2 = [];
+	for (let value = 0; value < 128; value++) {
 		const hexValue = value.toString(16).toUpperCase().padStart(2, '0');
 		const b1 = Math.floor(value / 16);
 		const b2 = value % 16;
 		const output = KEYSTROKE_COMMAND + KEY_NIBBLES[b1] + KEY_NIBBLES[b2];
-		setRule(rules_2_data, `${hexValue}`, `oo${hexValue}`, output, true);
-		setRule(rules_3_data1, `${hexValue}`, `oo${hexValue}qq`, output);
-		setRule(rules_3_data2, `${hexValue}`, `oopp${hexValue}`, output, true);
+		setRule(value_rules1, `${hexValue}`, `oo${hexValue}`, output, true);
+		setRule(value_rules1, `${hexValue}`, `oo${hexValue}pp`, output);
+		setRule(value_rules2, `${hexValue}`, `oopp${hexValue}`, output, true);
 	}
 
 	let bmtp =
 		`[Project]\n` +
 		`Version=1\n`;
 
-	bmtp += getPreset(0, `[2] status`, rules_2_status);
-	bmtp += getPreset(1, `[2] data`, rules_2_data);
-	bmtp += getPreset(2, `[3] status`, rules_3_status);
-	bmtp += getPreset(3, `[3] data 1`, rules_3_data1);
-	bmtp += getPreset(4, `[3] data 2`, rules_3_data2);
+	bmtp += getPreset(0, `Events`, message_rules);
+	bmtp += getPreset(1, `Values 1`, value_rules1);
+	bmtp += getPreset(2, `Values 2`, value_rules2);
 
 	process.stdout.write(bmtp);
 }
