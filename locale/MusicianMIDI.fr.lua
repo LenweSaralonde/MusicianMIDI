683384680bcfd805964f84076b6cff653741b8bf
LenweSaralonde <lenwe@lenwe.info>
2021-01-05 00:37:36 +0100

    French translation

--- locale/MusicianMIDI.fr.lua
+++ locale/MusicianMIDI.fr.lua
@@ -7,6 +7,9 @@ msg.MIDI_KEYBOARD_TITLE = "Clavier MIDI"
 msg.SELECT_INSTRUMENT = "Changer d'instrument"
 msg.INSTRUMENT_OCTAVE = "Octave"
 
+msg.SPLIT_KEYBOARD = "Diviser"
+msg.SET_SPLIT_KEY_HINT = "Appuyez sur la touche correspondant à la première note de la section supérieure sur votre clavier MIDI."
+
 msg.ERROR_INCOMPATIBLE_MUSICIAN_VERSION = "Votre version de Musician est trop ancienne pour Musician MIDI. Veuillez mettre à jour."
 
 if (GetLocale() == "frFR") then
