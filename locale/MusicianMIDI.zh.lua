61f1953da417088de0b7b26e558cd85d57af5770
Kalevalar <43042991+Kalevalar@users.noreply.github.com>
2021-01-06 01:49:08 +0100

    Chinese localization

--- locale/MusicianMIDI.zh.lua
+++ locale/MusicianMIDI.zh.lua
@@ -7,6 +7,9 @@ msg.MIDI_KEYBOARD_TITLE = "MIDI键盘"
 msg.SELECT_INSTRUMENT = "选择乐器"
 msg.INSTRUMENT_OCTAVE = "八度移调"
 
+msg.SPLIT_KEYBOARD = "分割"
+msg.SET_SPLIT_KEY_HINT = "按MIDI键盘左半部分的第一个键。"
+
 msg.ERROR_INCOMPATIBLE_MUSICIAN_VERSION = "你的Musician版本太旧以致不能使用Musician MIDI，请升级。"
 
 if (GetLocale() == "zhCN" or GetLocale() == "zhTW") then
