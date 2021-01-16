53dbd4ba61e5316d7f07d15fc77213fccce21472
LenweSaralonde <lenwe@lenwe.info>
2021-01-16 19:56:20 +0100

    Refactored locale files to ease up collaboration

--- locale/MusicianMIDI.zh.lua
+++ locale/MusicianMIDI.zh.lua
@@ -1,5 +1,4 @@
-MusicianMIDI.Locale.zh = Musician.Utils.DeepCopy(MusicianMIDI.Locale.en)
-local msg = MusicianMIDI.Locale.zh
+local msg = MusicianMIDI.InitLocale('zh', "中文", 'zhCN', 'zhTW')
 
 msg.MENU_MIDI_KEYBOARD = "启动MIDI键盘"
 msg.COMMAND_LIVE_KEYBOARD = "启动MIDI键盘"
@@ -11,7 +10,3 @@ msg.SPLIT_KEYBOARD = "分割"
 msg.SET_SPLIT_KEY_HINT = "按MIDI键盘左半部分的第一个键。"
 
 msg.ERROR_INCOMPATIBLE_MUSICIAN_VERSION = "你的Musician版本太旧以致不能使用Musician MIDI，请升级。"
-
-if (GetLocale() == "zhCN" or GetLocale() == "zhTW") then
-	MusicianMIDI.Msg = msg
-end
