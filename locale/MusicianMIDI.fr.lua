d81fa9a18c902c6601128413d1f1ba02d7c97db6
LenweSaralonde <lenwe@lenwe.info>
2021-05-02 17:37:20 +0200

    Refreshed localization

--- locale/MusicianMIDI.fr.lua
+++ locale/MusicianMIDI.fr.lua
@@ -1,4 +1,20 @@
-local msg = MusicianMIDI.InitLocale('fr', "Français", 'frFR')
+------------------------------------------------------------------------
+-- Please read the localization guide in the Wiki:
+-- https://github.com/LenweSaralonde/Musician/wiki/Localization
+--
+-- * Commented out msg lines need to be translated.
+-- * Do not translate anything on the left hand side of the = sign.
+-- * Do not translate placeholders in curly braces ({variable}).
+-- * Keep the text as a single line. Use \n for carriage return.
+-- * Escape double quotes (") with a backslash (\").
+-- * Check the result in game to make sure your text fits the UI.
+------------------------------------------------------------------------
+
+local msg = MusicianMIDI.InitLocale("fr", "Français", "frFR")
+
+------------------------------------------------------------------------
+---------------- ↑↑↑ DO NOT EDIT THE LINES ABOVE ! ↑↑↑  ----------------
+------------------------------------------------------------------------
 
 msg.MENU_MIDI_KEYBOARD = "Ouvrir le clavier MIDI"
 msg.COMMAND_LIVE_KEYBOARD = "Ouvrir le clavier MIDI"
