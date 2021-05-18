76698ba06c5853f811c8196549f38262d414f623
LenweSaralonde <lenwe@lenwe.info>
2021-05-18 19:08:28 +0200

    Added localization team in credits

--- locale/MusicianMIDI.base.lua
+++ locale/MusicianMIDI.base.lua
@@ -1,2 +1,17 @@
 MusicianMIDI.LocaleBase = {}
 local msg = MusicianMIDI.LocaleBase
+
+if Musician.LocalizationTeam then
+	tAppendAll(Musician.LocalizationTeam, {
+		{ 'de', Musician.DefaultTranslator },
+		{ 'en', "LenweSaralonde" },
+		{ 'es', Musician.DefaultTranslator },
+		{ 'fr', "LenweSaralonde" },
+		{ 'it', Musician.DefaultTranslator },
+		{ 'ko', Musician.DefaultTranslator },
+		{ 'pt', Musician.DefaultTranslator },
+		{ 'ru', Musician.DefaultTranslator },
+		{ 'tw', Musician.DefaultTranslator },
+		{ 'zh', "Grayson Blackclaw" },
+	})
+end
